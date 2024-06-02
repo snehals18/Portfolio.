4aaace22cdc938fdd32c9fad01a51d79d4e6edc8
snehalshirke.openinfotech@gmail.com <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>
2024-06-02 11:05:17 +0530

    first commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Angular Development"],
+        strings: ["Front-End Development"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
